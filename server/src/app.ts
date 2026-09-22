673ecaa47641dd236661c38342725e9d35364014
prabhatjaidiya <prabhatjaidiya@gmail.com>
2026-09-22 15:16:54 +0530

    fix: allow deployed frontend in CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,10 +10,10 @@ import errorMiddleware from "./middleware/error.middleware.js";
 const app = express();
 
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
 );
 
 app.use(express.json());
